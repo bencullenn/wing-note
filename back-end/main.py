b995d9a32ec40162a3f03c32a335d69b230ddc96
Ben Cullen <ben.cullen.n@gmail.com>
2025-02-14 22:51:14 -0800

    Created supabase client on server

--- back-end/main.py
+++ back-end/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 
 @app.get("/")
