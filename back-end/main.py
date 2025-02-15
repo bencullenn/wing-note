9cc8f2e69b4731aa817618d5caa4634702ff85fc
Ben Cullen <ben.cullen.n@gmail.com>
2025-02-14 23:06:41 -0800

    Added endpoints

--- back-end/main.py
+++ back-end/main.py
@@ -12,6 +12,31 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Get all visits
+@app.get("/visits")
+def get_visits():
+    visits = supabase.table("visits").select("*").execute()
+    return visits
+
+
+# Get visit for patient by id
+@app.get("/visits/{id}")
+def get_visit_by_id(id: int):
+    visits = supabase.table("visits").select("*").eq("id", id).execute()
+    return visits
+
+
+# Approve visit record by id
+@app.put("/visits/{visit_id}")
+def approve_visit(visit_id: int):
+    visit = supabase.table("visits").select("*").eq("id", visit_id).execute()
+    if visit["approved"] is False:
+        updated_visit = (
+            supabase.table("visits")
+            .update({"approved": True})
+            .eq("id", visit_id)
+            .execute()
+        )
+        return updated_visit
+    else:
+        return {"message": "Visit is already approved"}
